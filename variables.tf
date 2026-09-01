64a01be9d07d804e505e4a21a5a351b3540289e9
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-01 21:04:34 +0100

    Add multiple variables for AWS and GitHub configuration

--- variables.tf
+++ variables.tf
@@ -1,1 +1,34 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
 
+variable "github_org" {
+  description = "GitHub Organization or Username"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub Repository Name"
+  type        = string
+}
+
+variable "github_branch" {
+  description = "GitHub Branch allowed to assume role"
+  type        = string
+}
+
+variable "eks_cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+}
+
+variable "ecr_repository_name" {
+  description = "ECR Repository Name"
+  type        = string
+}
+
+variable "role_name" {
+  description = "GitHub OIDC Role Name"
+  type        = string
+}
